f91b1324c7591b8889bb2da6ea7145212282306b
Brad Bao <brad_bao@m018647.aw.org>
2022-10-26 08:10:32 -0700

    Python Problem Set #2

---
+++ pythonprobset2.py
@@ -0,0 +1,31 @@
+def factorial(x):
+    y = 1
+    for i in range(1, x + 1):
+        y = y * i
+
+    print ("The factorial of " + str(x) + " is ",end="")
+    print (y)
+
+def doubleIt():
+    word = input("Write the phrase you want doubled:\n")
+    output = ""
+    for char in word:
+        output = output + char * 2
+    print(output)
+
+def camelCase():
+    file = input("Please enter filename:\n")
+    ?????????????
+    
+def main():
+    factorial(1)
+    factorial(12)
+    factorial(6)
+
+    doubleIt()
+
+    camelCase()
+
+
+if __name__ == "__main__":
+    main()
